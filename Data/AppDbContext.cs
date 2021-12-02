cb6b72317f9242caf629c46ed1635700d3737fe9
dhanabal <dhanabalap@gmail.com>
2021-12-02 21:12:10 +0530

    In_Memory DB Context Sprin2

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using GL_DotNetFullStack_Project.Models;
+
+namespace GL_DotNetFullStack_Project.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> optins) :base(optins)//same thing canbe passed to parent class constructor
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+
+    }
+}
