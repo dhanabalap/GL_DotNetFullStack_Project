9056348557aedca62a422dccce26296cdd21e1f5
dhanabal <dhanabalap@gmail.com>
2021-12-18 16:16:58 +0530

    Test xUnit project added and sln altered

---
+++ TestxUnit_Project/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TestxUnit_Project
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
